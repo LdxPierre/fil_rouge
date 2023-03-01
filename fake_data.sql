38bc58452750661f1968e3f65328632c61ade2f9
Pierre <pierre.ledoux@outlook.fr>
2023-03-01 16:34:55 +0100

    create fake data

---
+++ fake_data.sql
@@ -0,0 +1,13 @@
+USE green_village;
+
+INSERT INTO `employee` (`lastname`, `firstname`, `password`, `title`, `team`, `email`, `phone`, `leader`) VALUES
+	('Turner', 'Christian', 'samsung', 'random', 'random', 'christian.turner@example.com', '(935) 310-5593', NULL),
+	('Robertson', 'Wallace', 'marathon', 'random', 'random', 'wallace.robertson@example.com', '(993) 304-9688', NULL),
+	('Berry', 'Eugene', 'ttttttt', 'random', 'random', 'eugene.berry@example.com', '(419) 208-9953', NULL),
+	('Morales', 'Serenity', 'comanche', 'random', 'random', 'serenity.morales@example.com', '(380) 403-5182', NULL),
+	('Webb', 'Wesley', 'tester', 'random', 'random', 'wesley.webb@example.com', '(514) 241-4363', NULL),
+	('Morris', 'Pamela', 'thegreat', 'random', 'random', 'pamela.morris@example.com', '(652) 457-6327', NULL),
+	('Patterson', 'Roberta', 'lakers1', 'random', 'random', 'roberta.patterson@example.com', '(532) 762-4420', NULL),
+	('Beck', 'Dora', 'serenity', 'random', 'random', 'dora.beck@example.com', '(860) 601-6822', NULL),
+	('Davidson', 'Russell', 'marisa', 'random', 'random', 'russell.davidson@example.com', '(640) 548-1312', NULL),
+	('Owens', 'Rosa', 'alan', 'random', 'random', 'rosa.owens@example.com', '(849) 664-5726', NULL);
